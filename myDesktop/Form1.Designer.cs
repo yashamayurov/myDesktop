61aa8a8374d4489ba997df4d3cbdbcf33b7d9104
yashamayurov <64410504+yashamayurov@users.noreply.github.com>
2022-07-01 08:33:08 +0300

    Update code

--- myDesktop/Form1.Designer.cs
+++ myDesktop/Form1.Designer.cs
@@ -28,9 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelComName = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Location = new System.Drawing.Point(22, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(956, 944);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.AutoSize = true;
+            this.panel2.Controls.Add(this.labelTime);
+            this.panel2.Controls.Add(this.labelDate);
+            this.panel2.Controls.Add(this.labelComName);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 931);
+            this.panel2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.panel2.MinimumSize = new System.Drawing.Size(0, 130);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2292, 130);
+            this.panel2.TabIndex = 6;
+            // 
+            // labelTime
+            // 
+            this.labelTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(2095, 54);
+            this.labelTime.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(67, 24);
+            this.labelTime.TabIndex = 5;
+            this.labelTime.Text = "Время";
+            this.labelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelDate
+            // 
+            this.labelDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelDate.AutoSize = true;
+            this.labelDate.Location = new System.Drawing.Point(1677, 54);
+            this.labelDate.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(54, 24);
+            this.labelDate.TabIndex = 4;
+            this.labelDate.Text = "Дата";
+            this.labelDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelComName
+            // 
+            this.labelComName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelComName.AutoSize = true;
+            this.labelComName.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelComName.Location = new System.Drawing.Point(280, 52);
+            this.labelComName.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.labelComName.Name = "labelComName";
+            this.labelComName.Size = new System.Drawing.Size(60, 24);
+            this.labelComName.TabIndex = 3;
+            this.labelComName.Text = "label1";
             // 
             // button1
             // 
@@ -38,48 +113,45 @@
             this.button1.BackColor = System.Drawing.Color.White;
             this.button1.Image = global::myDesktop.Properties.Resources.png_clipart_power_on_off_logo_computer_icons_shutdown_internet_computer_software_quit_miscellaneous_web_design_thumbnail__1_;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(12, 381);
+            this.button1.Location = new System.Drawing.Point(22, 34);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 35);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(194, 65);
+            this.button1.TabIndex = 2;
             this.button1.Text = "Выключить";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(151, 377);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 39);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(2292, 1061);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelComName;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
--- myDesktop/Form1.cs
+++ myDesktop/Form1.cs
@@ -1,38 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
+using System.Threading;
 
 namespace myDesktop
 {
     public partial class Form1 : Form
     {
+
+        private Process proc = new Process();
         public Form1()
         {
             InitializeComponent();
+            resfreshButtons();
+            labelComName.Text = "Имя терминальной станции:" + Environment.MachineName;
         }
         private void ButtonOnClick(object sender, EventArgs eventArgs)
         {
             Button button = (Button)sender;
-            string filename = button.Tag.ToString();
-            var proc = new System.Diagnostics.Process();
-            proc.StartInfo.FileName = filename;
+            string filename = "\""+button.Tag.ToString()+"\"";
+            proc.StartInfo.FileName = "mstsc.exe";
+            proc.StartInfo.Arguments = filename;
             proc.StartInfo.UseShellExecute = true;
+            proc.EnableRaisingEvents = true;
             proc.Start();
+            this.Visible = false;
+            Thread.Sleep(1000);
+            int idProc = proc.Id;
+            
+            MessageBox.Show(idProc.ToString());
+
+            int i = 1;
+            while (i>0)
+            {
+                  Process[] procs = Process.GetProcessesByName("mstsc");
+                  i = procs.Length;
+            }
+            this.Visible = true;
         }
-        private void Form1_Activated(object sender, EventArgs e)
+        private void resfreshButtons()
         {
 
-            label1.Text = "Имя терминальной станции:"+ Environment.MachineName;
+
+            
             string myPath = Environment.GetCommandLineArgs()[1];
             string[] allfiles = Directory.GetFiles(myPath);
-            int top = 20;
             int left = 10;
             int height = 40;
             int width = 500;
@@ -40,17 +53,22 @@ namespace myDesktop
             foreach (string filename in allfiles)
             {
                 Button button = new Button();
+
+
                 button.Left = left;
-                button.Top = top * i;
-                button.Name = "btn" + i;
+                button.Top = height * i;
+                button.Name = "btn" + i.ToString();
                 button.Width = width;
                 button.Height = height;
                 button.Text = Path.GetFileNameWithoutExtension(filename);
+ 
                 button.Tag = filename;
                 button.Click += ButtonOnClick;
 
-                this.Controls.Add(button);
-                top += button.Height + 2;
+                panel1.Controls.Add(button);
+           
+
+                i++;
             }
         }
 
@@ -61,8 +79,35 @@ namespace myDesktop
 
         private void button1_Click(object sender, EventArgs e)
         {
+        
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //resfreshButtons();
+        }
+
+       
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            labelTime.Text = DateTime.Now.ToShortTimeString();
+            labelDate.Text = DateTime.Today.ToShortDateString();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
             System.Diagnostics.Process.Start("cmd", "/c shutdown -s -t 3");
             Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
